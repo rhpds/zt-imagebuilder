4ca479f741a75424f245c357a46f8c41483d8e3b
Matthew Yee <myee@redhat.com>
2025-08-28 12:27:48 -0700

    Add README and new images; update installation and blueprint documentation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Important
+
+This lab is isolated from the internet. Image builder must be configured to use the RHDP satellite server. Follow these [instructions](https://access.redhat.com/solutions/5773421).
--- runtime-automation/02-login/setup-rhel.sh
+++ runtime-automation/02-login/setup-rhel.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 echo "Starting module called 02-login" >> /tmp/progress.log
+
+mkdir -pvm 0755 /etc/osbuild-composer/repositories
+cp /usr/share/osbuild-composer/repositories/*.json /etc/osbuild-composer/repositories/
+sed -i -e 's|cdn.redhat.com|demosat-ha.infra.demo.redhat.com/pulp/repos/Red_Hat_RHDP_Labs/Library/zt-rhelbu|' /etc/osbuild-composer/repositories/rhel-10.json
+systemctl restart osbuild-worker@.service.d osbuild-worker@1.service osbuild-composer.service
