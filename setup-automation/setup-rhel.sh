46bc74c01f11fc239a13cc69ddd31fa75a0f65a1
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-06-22 12:05:06 -0400

    Fix setup-rhel.sh comment placement

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Enable cockpit functionality in showroom.
+
 # Unregister and register the VM
 subscription-manager clean
 subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
 
+# Enable cockpit functionality in showroom.
 echo "[WebService]" > /etc/cockpit/cockpit.conf
 echo "Origins = https://cockpit-${GUID}.${DOMAIN}" >> /etc/cockpit/cockpit.conf
 echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
