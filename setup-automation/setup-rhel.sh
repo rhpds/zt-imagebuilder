456328a0df1db17c47ad2369aa56b4b351426fc5
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-06-22 10:23:19 -0400

    Unregister and re-register the system

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Enable cockpit functionality in showroom.
+# Unregister and register the VM
+subscription-manager clean
+subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
+
 echo "[WebService]" > /etc/cockpit/cockpit.conf
 echo "Origins = https://cockpit-${GUID}.${DOMAIN}" >> /etc/cockpit/cockpit.conf
 echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
