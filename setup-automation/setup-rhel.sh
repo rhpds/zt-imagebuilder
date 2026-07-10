83b42e5f36703241611f32f8fc48c7cb35ad3831
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-07-10 12:45:10 -0400

    Installing cockpit

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -5,6 +5,9 @@ dnf -y remove katello-ca-consumer-*
 subscription-manager clean
 subscription-manager register --activationkey=$ACTIVATION_KEY --org=$ORG_ID --force
 
+# Install cockpit
+dnf install -y cockpit
+
 # Enable cockpit functionality in showroom.
 echo "[WebService]" > /etc/cockpit/cockpit.conf
 echo "Origins = https://cockpit-${GUID}.${DOMAIN}" >> /etc/cockpit/cockpit.conf
